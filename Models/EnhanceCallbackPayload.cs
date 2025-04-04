079793bde22d917172942e4dd8a1dd8bff2d8a68
Peter Grigor <petrupewdiepie@gmail.com>
2025-04-04 08:03:11 +0300

    fixed callback payload

--- Models/EnhanceCallbackPayload.cs
+++ Models/EnhanceCallbackPayload.cs
@@ -1,54 +1,69 @@
+using System;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
-namespace PhotoAiBackend.Models;
-
-public class EnhanceCallbackPayload
+namespace PhotoAiBackend.Models
 {
-    [JsonPropertyName("id")]
-    public string Id { get; set; }
+    public class EnhanceCallbackPayload
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; }
 
-    [JsonPropertyName("version")]
-    public string Version { get; set; }
+        [JsonPropertyName("model")]
+        public string Model { get; set; }
 
-    [JsonPropertyName("status")]
-    public string Status { get; set; }
+        [JsonPropertyName("version")]
+        public string Version { get; set; }
 
-    [JsonPropertyName("created_at")]
-    public DateTime CreatedAt { get; set; }
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
 
-    [JsonPropertyName("started_at")]
-    public DateTime StartedAt { get; set; }
+        [JsonPropertyName("created_at")]
+        public DateTime CreatedAt { get; set; }
 
-    [JsonPropertyName("completed_at")]
-    public DateTime CompletedAt { get; set; }
+        [JsonPropertyName("input")]
+        public PredictionInput Input { get; set; }
 
-    [JsonPropertyName("input")]
-    public PredictionInput Input { get; set; }
+        [JsonPropertyName("output")]
+        public List<string>? Output { get; set; }
 
-    [JsonPropertyName("output")]
-    public List<string> Output { get; set; }
+        [JsonPropertyName("logs")]
+        public string? Logs { get; set; }
 
-    [JsonPropertyName("metrics")]
-    public PredictionMetrics Metrics { get; set; }
-}
+        [JsonPropertyName("data_removed")]
+        public bool DataRemoved { get; set; }
 
-public class PredictionInput
-{
-    [JsonPropertyName("input")]
-    public string InputImage { get; set; }
+        [JsonPropertyName("error")]
+        public string? Error { get; set; }
 
-    [JsonPropertyName("background_upsampler")]
-    public string BackgroundUpsampler { get; set; }
+        [JsonPropertyName("urls")]
+        public PredictionUrls Urls { get; set; }
+    }
 
-    [JsonPropertyName("upscaling_model_type")]
-    public string UpscalingModelType { get; set; }
+    public class PredictionInput
+    {
+        [JsonPropertyName("input")]
+        public string InputImage { get; set; }
 
-    [JsonPropertyName("super_resolution_factor")]
-    public int SuperResolutionFactor { get; set; }
-}
+        [JsonPropertyName("background_upsampler")]
+        public string BackgroundUpsampler { get; set; }
 
-public class PredictionMetrics
-{
-    [JsonPropertyName("predict_time")]
-    public double PredictTime { get; set; }
+        [JsonPropertyName("upscaling_model_type")]
+        public string UpscalingModelType { get; set; }
+
+        [JsonPropertyName("super_resolution_factor")]
+        public int SuperResolutionFactor { get; set; }
+    }
+
+    public class PredictionUrls
+    {
+        [JsonPropertyName("cancel")]
+        public string Cancel { get; set; }
+
+        [JsonPropertyName("get")]
+        public string Get { get; set; }
+
+        [JsonPropertyName("stream")]
+        public string Stream { get; set; }
+    }
 }
--- obj/Debug/net8.0/PhotoAiBackend.AssemblyInfo.cs
+++ obj/Debug/net8.0/PhotoAiBackend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PhotoAiBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac037d0add285be3fa3c6198c917cd3b3443d68c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22af40a09839e47fef8a263487912073484e183f")]
 [assembly: System.Reflection.AssemblyProductAttribute("PhotoAiBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PhotoAiBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
